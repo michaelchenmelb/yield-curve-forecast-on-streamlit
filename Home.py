3a207d799b2b93554d70f6eab8a19329fb6c3afb
MichaelChenMelb <53508277+michaelchenmelb@users.noreply.github.com>
2024-02-04 23:26:26 +0000

    update

--- Home.py
+++ Home.py
@@ -12,6 +12,22 @@ def run():
 
     st.write("# Yield Curve Forecast on Streamlit")
 
+    st.markdown(
+        """
+        **Streamlit:**
+
+        Explore the capabilities of Streamlit, a platform designed for streamlined data exploration. This interface offers a balance of simplicity and sophistication, providing a professional experience accessible to users of various technical backgrounds.
+
+        **Yield Curve Forecasting:**
+
+        Delve into the world of Yield Curve Forecasting, focusing on the U.S. Yield Curve. This crucial economic indicator offers insights into future market trends. The user-friendly interface facilitates easy navigation and forecasting, making financial analysis an enlightening experience for all users. Welcome to a space where professionalism and approachable finance intersect.
+
+    
+    
+    """
+    )
+
+
 if __name__ == "__main__":
 
     run()
